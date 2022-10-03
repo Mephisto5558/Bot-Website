7f694ed7fcb51e21fd044c0ff8a41cbc37a66cbf
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-03 15:14:48 +0200

    added embed builder

---
+++ CustomSites/tools/embedbuilder/builder.config.js
@@ -0,0 +1,7 @@
+options = {
+  username: 'Teufelsbot',
+  avatar: 'https://cdn.discordapp.com/avatars/948978571802710047/05c4c0ce9518bb0a82fe46718ac02695.webp?size=256',
+  verified: true,
+  guiTabs: ['description'],
+  multiEmbeds: true
+};
