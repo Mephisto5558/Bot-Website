1fd9efb73a75054e421232f8b6eefac9d6255b00
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-17 11:45:41 +0200

    dark mode

--- CustomSites/dev/todo.js
+++ CustomSites/dev/todo.js
@@ -1,4 +1,4 @@
 export default {
   title: 'Dev ToDo List',
-  run: res => res.send('<iframe src="https://docs.google.com/document/d/e/2PACX-1vSP4bpVt4IyUHoJcCN-e_DUKHrBEe4-JwFSUnMpU4s6HtyiidR7okgMSK4xJshFY50qcWpFy1Uve4Eo/pub?embedded=true" style="position: fixed;width:100%;height:100%;" frameborder="0">Loading…</iframe>')
+  run: res => res.send('<body style="background-color:#1e1e1e"><iframe src="https://docs.google.com/document/d/e/2PACX-1vSP4bpVt4IyUHoJcCN-e_DUKHrBEe4-JwFSUnMpU4s6HtyiidR7okgMSK4xJshFY50qcWpFy1Uve4Eo/pub?embedded=true" style="position: fixed;width:100%;height:100%;" frameborder="0">Loading…</iframe></body>')
 };
