8e617d8bdd89e243e8e66bc5d66ac3968552f8b6
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-02-17 00:19:30 +0100

    moved todo list

--- CustomSites/dev/todo.js
+++ CustomSites/dev/todo.js
@@ -1,4 +1,4 @@
 export default {
   title: 'Dev ToDo List',
-  run: res => res.send('<body style="background-color:#1e1e1e"><iframe src="https://docs.google.com/document/d/e/2PACX-1vSP4bpVt4IyUHoJcCN-e_DUKHrBEe4-JwFSUnMpU4s6HtyiidR7okgMSK4xJshFY50qcWpFy1Uve4Eo/pub?embedded=true" style="position: fixed;width:100%;height:100%;" frameborder="0">Loading…</iframe></body>')
+  run: res => res.redirect('https://github.com/users/Mephisto5558/projects/1')
 };
--- CustomSites/dev/todo/edit.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: 'ToDo List',
-  run: res => res.redirect('https://docs.google.com/document/d/1XVu1Jr9MA0w7Tq2UQ00JmjXaefKTCE85yNO8M3sxphY/edit')
-};
