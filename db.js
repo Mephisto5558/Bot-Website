c9027185913cfd1ec9e04e49b47642b5a3d75728
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-03-13 14:04:07 +0100

    updated code, removed dashboard functionality for now, will rewrite it

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 import Mongoose from 'mongoose';
 import { get } from 'http';
+Mongoose.set('strictQuery', true);
 
 export default class DB {
   /**@param {string}dbConnectionString MongoDB connection string*/
   constructor(dbConnectionString) {
-    if (Mongoose.connection.readyState !== 1) {
+    if (Mongoose.connection.readyState != 1) {
       if (!dbConnectionString) throw new Error('A Connection String is required!');
-      return Mongoose.connect(dbConnectionString).then(() => { return this; });
+      Mongoose.connect(dbConnectionString);
     }
   }
 
@@ -15,64 +16,65 @@ export default class DB {
     value: Mongoose.SchemaTypes.Mixed
   }));
 
+  /**@returns value of collection*/
   get = key => this.schema.findOne({ key }).then(e => e.value);
 
-  set(key, value) {
+  /**@param {string}key@param {boolean}overwrite overwrite existing collection, default: `false`*/
+  async set(key, value, overwrite = false) {
     if (!key) return;
-    this.schema.findOne({ key }, (err, data) => {
-      if (err) throw err;
-      if (data) data.value = value;
-      else data = new this.schema({ key, value });
+    let data;
 
-      data.save();
-      if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
-    });
+    if (!overwrite) data = await this.schema.findOne({ key });
+    if (data) data.value = value;
+    else data = new this.schema({ key, value });
+
+    await data.save();
+    if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
   }
 
   /**@param {string}db@param {string}key*/
-  update(db, key, value) {
+  async update(db, key, value) {
     if (!key) return;
     if (typeof key != 'string') throw new Error(`key must be typeof string! Got ${typeof key}.`);
 
-    this.schema.findOne({ key: db }, (err, data) => {
-      if (err) throw err;
-      if (data && typeof data.value != 'object') throw new Error(`data.value in db must be typeof object! Found ${typeof data.value}.`);
-      if (!data) data = new this.schema({ key, value: {} });
-      DB.mergeWithFlat(data.value, key, value);
+    const data = await this.schema.findOne({ key: db }) || new this.schema({ key: db, value: {} });
 
-      data.markModified(`value.${key}`);
-      data.save();
-      if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
-    });
+    data.value ??= {};
+    if (typeof data.value != 'object') throw new Error(`data.value in db "${db}" must be typeof object! Found ${typeof data.value}.`);
+
+    DB.mergeWithFlat(data.value, key, value);
+
+    data.markModified(`value.${key}`);
+    await data.save();
+    if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
   }
 
-  push(key, ...pushValue) {
-    if (!pushValue?.length) return;
-    this.schema.findOne({ key }, (err, res) => {
-      if (err) throw err;
-      if (!Array.isArray(res.value)) throw Error(`You cant push data to a ${typeof data} value!`);
+  /**@param {string}key*/
+  async push(key, ...pushValue) {
+    const values = pushValue.flat();
 
-      res.value = [...res.value, ...pushValue.flat()];
-      res.save();
-      if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
-    });
+    const data = await this.schema.findOne({ key }) ?? new this.schema({ key, value: pushValue });
+    if (data.value && !Array.isArray(data.value)) throw Error(`You can't push data to a ${typeof data.value} value!`);
+    data.value = data.value ? [...data.value, ...values] : pushValue;
+
+    await data.save();
+    if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
   }
 
-  delete(key) {
+  /**@param {string}key*/
+  async delete(key) {
     if (!key) return;
-    this.schema.findOne({ key }, async (err, data) => {
-      if (err) throw err;
-      if (data) {
-        await data.delete();
-        if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
-      }
-    });
+
+    const data = await this.schema.findOne({ key });
+    if (data) {
+      await data.delete();
+      if (process.env.BotUpdateDBURL) get(process.env.BotUpdateDBURL + `&db=${key}`);
+    }
   }
 
-  /**@param {{}}obj gets mutated! @param {string}key@example DB.mergeWithFlat({a: {b:1} }, 'a.c', 2):{a: {b:1, c:2}}*/
+  /**@param {object}obj gets mutated! @param {string}key@returns reduce return value @example DB.mergeWithFlat({a: {b:1}}, 'a.c', 2)*/
   static mergeWithFlat(obj, key, val) {
     const keys = key.split('.');
-    keys.reduce((acc, e, i) => acc[e] = keys.length - 1 == i ? val : acc[e] ?? {}, obj);
-    return obj;
+    return keys.reduce((acc, e, i) => acc[e] = keys.length - 1 == i ? val : acc[e] ?? {}, obj);
   }
-}
+};
