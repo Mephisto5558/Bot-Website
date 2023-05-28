59d98a0d708d1f752fedb6482cf5c71a5b9459e9
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-28 19:03:04 +0200

    updated to use fs/promises, added dev boolean

--- CustomSites/api/v1/internal/user.js
+++ CustomSites/api/v1/internal/user.js
@@ -1,3 +1,6 @@
+import { readFile } from 'fs/promises';
+const { devIds } = JSON.parse(await readFile('./config.json', 'utf-8').catch(() => ({}))) || {};
+
 export default {
-  run: (res, req) => res.json(req.user ?? { error: 401 })
+  run: async (res, req) => req.user ? res.json({ ...req.user, dev: devIds?.includes(req.user.id) }) : res.json({ error: 401 })
 };
