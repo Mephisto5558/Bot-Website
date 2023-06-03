7e2d144d268f5e21bba7ee3e86c98c08f9ad7664
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-03 16:13:30 +0200

    updated code

--- CustomSites/api/v1/internal/vote/delete.js
+++ CustomSites/api/v1/internal/vote/delete.js
@@ -1,3 +1,3 @@
 export default {
-  run: function (res, req) { return this.voteSystem.delete(req.query.id).then(e => res.status(e.errorCode || 200).json(e)); }
+  run: function (res, req) { return this.voteSystem.delete(req.query.featureId, req.query.userId).then(e => res.status(e.errorCode || 200).json(e)); }
 };
