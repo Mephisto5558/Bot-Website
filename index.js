fe25b25b96cf413d39b1d8b210282c7f4c1d9eff
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-08 01:49:46 +0200

    moved the dashboard code to its own file, currently just returning an error msg, "fixed" login on replit

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ console.time('Initializing time');
 
 import { appendFile, access, readdir, readFile } from 'fs/promises';
 import { inspect } from 'util';
-// import fetch from 'node-fetch';
 import express from 'express';
 import { Client, GatewayIntentBits } from 'discord.js';
-// import DBD from 'discord-dashboard';
+import createDashboard from './dashboard.js';
 import DB from './Utils/db.js';
 import VoteSystem from './Utils/VoteSystem.js';
-// import SoftUITheme from 'dbd-soft-ui'; //Currently disabled because it requires quick.db wich doesn't run on Node 19
 import passport from 'passport';
 import Strategy from 'passport-discord';
 import compression from 'compression';
@@ -20,27 +18,11 @@ import session from 'express-session';
 import cors from 'cors';
 import path from 'path';
 import escapeHTML from 'escape-html';
-// import Settings from './settings.js';
 
 function error(err, req, res) {
   console.error(err);
   appendFile('./errorlog.log', `[${new Date().toLocaleTimeString('en', { timeStyle: 'medium', hour12: false }).replace(/^24:/, '00:')}] Err: ${JSON.stringify(err)}\nREQ: ${JSON.stringify(inspect(req))}}\nRES: ${JSON.stringify(inspect(res))}`);
 }
-
-// async function getCommands() {
-//   if (process.env.BotCommandListURL) {
-//     try {
-//       const data = await fetch(process.env.BotCommandListURL).then(e => e.json());
-//       return Array.isArray(data) ? data : [];
-//     }
-//     catch (err) {
-//       if (err.constructor.name == 'FetchError') console.error(`FetchError: Couldn't connect to process.env.BotCommandListURL: ${err.code}`);
-//       else throw err;
-//     }
-//   }
-//   else console.warn('process.env.BotCommandListURL is not defined. Not setting commands in the dashboard page.');
-//   return [];
-// }
 
 /**@param {string}filepath full path*/
 async function importFile(filepath) {
@@ -74,93 +56,14 @@ if (!/^https?:\/\//.test(domain)) {
   domain = 'http://' + domain;
 }
 
-// await DBD.useLicense(Keys.dbdLicense);
 await client.login(Keys.token);
 
 client.db = new DB(Keys.dbConnectionStr);
 client.voteSystem = await new VoteSystem(client.db).init();
-// client.dashboardOptionCount = [];
 while (client.ws.status) await new Promise(r => setTimeout(r, 10));
 await client.application.fetch();
 
-// global.embedBuilder = DBD.formTypes.embedBuilder({
-//   username: client.user.username,
-//   avatarURL: client.user.displayAvatarURL({ forceStatic: true }),
-//   defaultJson: {}
-// });
-
-// const Dashboard = new (DBD.UpdatedClass())({
-//   port, domain,
-//   acceptPrivacyPolicy: true,
-//   minimizedConsoleLogs: true,
-//   noCreateServer: true,
-//   useUnderMaintenance: false,
-//   useCategorySet: true,
-//   html404: await readFile('./CustomSites/error/404.html', 'utf-8'),
-//   redirectUri: `${domain}/discord/callback`,
-//   bot: client,
-//   seesionStore: 'connect-mongodb-session',
-//   ownerIDs: [client.application.owner.id],
-//   client: {
-//     id: client.user.id,
-//     secret: Keys.secret
-//   },
-//   invite: {
-//     scopes: ['bot', 'applications.commands'],
-//     permissions: '412317240384'
-//   },
-//   theme: SoftUITheme({
-//     information: {
-//       createdBy: client.application.owner.tag,
-//       iconURL: client.user.displayAvatarURL(),
-//       websiteTitle: `${client.user.username} | Dashboard`,
-//       websiteName: `${client.user.username} | Dashboard`,
-//       websiteUrl: domain,
-//       dashboardUrl: domain,
-//       supporteMail: Support.Mail,
-//       supportServer: Support.Discord,
-//       imageFavicon: client.user.displayAvatarURL(),
-//       pageBackGround: 'linear-gradient(#2CA8FF, #155b8d)',
-//       preloader: 'Loading...',
-//       loggedIn: 'Successfully signed in.',
-//       mainColor: '#2CA8FF',
-//       subColor: '#ebdbdb'
-//     },
-//     index: {
-//       card: {
-//         category: `${client.user.username} Dashboard - The center of everything`,
-//         title: 'Welcome to the Teufelsbot dashboard where you can control the features and settings of the bot.',
-//         description: 'Look up commands and configurate servers on the left side bar!',
-//         image: 'https://i.imgur.com/axnP93g.png'
-//       },
-//       information: {},
-//       feeds: {},
-//     },
-//     commands: await getCommands()
-//   }),
-//   underMaintenance: {
-//     title: 'Under Maintenance',
-//     contentTitle: '<p id="content-title" style="color: #ddd9d9">This page is under maintenance</p>',
-//     texts: [
-//       '<br><p class="text" style="color: #ddd9d9">' +
-//       'We still want to change for the better for you.<br>' +
-//       'Therefore, we are introducing technical updates so that we can allow you to enjoy the quality of our services.' +
-//       '<br></p><br>'
-//     ],
-//     bodyBackgroundColors: ['#999', '#0f173d'],
-//     buildingsColor: '#6a6a6a',
-//     craneDivBorderColor: '#6a6a6a',
-//     craneArmColor: '#8b8b8b',
-//     craneWeightColor: '#8b8b8b',
-//     outerCraneColor: '#6a6a6a',
-//     craneLineColor: '#6a6a6a',
-//     craneCabinColor: '#8b8b8b',
-//     craneStandColors: ['#6a6a6a', undefined, '#f29b8b']
-//   },
-//   settings: await Settings.call(client)
-// });
-
-// await Dashboard.init();
+const Dashboard = await createDashboard(client, Keys.dbdLicense, Keys.secret, port, domain, Support);
 
 passport.serializeUser((user, done) => done(null, {
   id: user.id, username: user.username,
@@ -207,10 +110,11 @@ express()
       resave: false,
       saveUninitialized: false,
       store,
-      cookie: {
+      cookie: domain.includes('repl.co') ? undefined : {
         secure: domain.startsWith('https'),
         httpOnly: true,
         domain,
+        path: '*'
       }
     }),
     passport.initialize(),
