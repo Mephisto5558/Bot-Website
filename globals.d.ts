245d5c0e8ba359b509fd6b89ba48218182494ca8
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-04 15:36:21 +0100

    updated globals.d.ts and index.js: - index.js has now a WebServer class [NOT TESTED, WIP] and uses require (way better then import)

--- globals.d.ts
+++ globals.d.ts
@@ -1,17 +1,8 @@
 import type express from 'express'
 import type Discord from 'discord.js'
-import type DB from './Utils/db'
-import type VoteSystem from './Utils/VoteSystem'
 
 declare global {
   type Req = express.Request;
   type Res = express.Response;
   type Client = Discord.Client;
-}
-
-declare module 'discord.js' {
-  interface BaseClient {
-    db: DB;
-    voteSystem: VoteSystem;
-  }
 }
