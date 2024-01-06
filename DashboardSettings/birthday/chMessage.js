d67a0d04207e0597d0e81af15c2151d8ff21a093
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-07 00:02:36 +0100

    removed files

--- DashboardSettings/birthday/chMessage.js
+++
@@ -1,7 +0,0 @@
-export default {
-  id: 'chMsg',
-  name: 'Announcement Message',
-  description: "The message to send on the user's birthday",
-  type: global.embedBuilder,
-  position: 3
-}
--- DashboardSettings/birthday/dmEnable.js
+++
@@ -1,9 +0,0 @@
-import DDB from 'discord-dashboard';
-
-export default {
-  id: 'dmEnable',
-  name: 'Enable dm messages',
-  description: 'DM the member on his/her birthday with a custom message',
-  type: DDB.formTypes.switch(),
-  position: 4
-}
