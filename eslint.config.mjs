e6a58fc94a9b526e9fdc0c020db67625fbf289a4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-04 15:47:30 +0100

    added typing info to eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
 import config from '@mephisto5558/eslint-config';
 
+/**
+ * @type { import('eslint').Linter.Config[] }
+ * This config lists all rules from every plugin it uses.*/
 export default [...config];
